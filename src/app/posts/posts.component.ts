f248f3dd4dba4f162a1baccad2b673329169be68
Adam Jennings <ajennings@tier2consulting.com>
2021-07-30 10:26:24 +0100

    Updating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -26,4 +26,12 @@ export class PostsComponent {
       });
   }
 
+  updatePost(post: any){
+    this.http.patch(this.url + '/' + post.id,
+        JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
 }
