9591dbd2c362ca8559a2df5ed54f2ad4b8a21af6
Adam Jennings <ajennings@tier2consulting.com>
2021-08-06 11:53:13 +0100

    Adding an Object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {AngularFireDatabase} from "@angular/fire/database";
+import {AngularFireDatabase, AngularFireList} from "@angular/fire/database";
 import {Observable} from "rxjs";
 
 @Component({
@@ -12,10 +12,28 @@ export class AppComponent{
   course: Observable<any>;
   author: Observable<any>;
 
+  itemsRef: AngularFireList<any>;
+
   constructor(db: AngularFireDatabase) {
     this.courses = db.list('/courses').valueChanges();
     this.course = db.object('/courses/1').valueChanges();
     this.author = db.object('/authors/1').valueChanges();
+
+    this.itemsRef = db.list('courses');
+  }
+
+  add(course: HTMLInputElement){
+    this.itemsRef.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        {title: 'Components'},
+        {title: 'Directives'},
+        {title: 'Templates'},
+      ]
+    });
+    course.value = '';
   }
 
 }
