70c5b9b67be50c7bc26258ed1a880b67a01eb478
Adam Jennings <ajennings@tier2consulting.com>
2021-08-16 10:48:55 +0100

    Displaying the List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFireDatabase} from "@angular/fire/database";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,12 @@ export class ProductService {
 
   create(product: any){
     this.db.list('/products').push(product);
+  }
+  getAll(){
+
+    return this.db.list('/products')
+      .snapshotChanges()
+      // @ts-ignore
+      .pipe(map(actions => actions.map(a => ({ key: a.key, ...a.payload.val() }))));
   }
 }
