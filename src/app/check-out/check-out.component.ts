9bf597c9e67401d6b3f07837054e030a994175e5
Adam Jennings <ajennings@tier2consulting.com>
2021-08-18 12:34:03 +0100

    Associating Order with Current User

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -3,6 +3,7 @@ import {ShoppingCartService} from "../shopping-cart.service";
 import {ShoppingCart} from "../models/shopping-cart";
 import {Subscription} from "rxjs";
 import {OrderService} from "../order.service";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'check-out',
@@ -13,22 +14,28 @@ export class CheckOutComponent implements OnInit, OnDestroy{
   product:any;
   shipping:any = {};
   cart!: ShoppingCart;
-  subscription!: Subscription;
+  cartSubscription!: Subscription;
+  userId!: string;
+  userSubscription!: Subscription;
 
-  constructor(private cartService: ShoppingCartService,
+  constructor(private authService: AuthService,
+              private cartService: ShoppingCartService,
               private orderService: OrderService) { }
 
   async ngOnInit() {
     let cart$ = await this.cartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
   placeOrder() {
     // console.log(this.shipping);
     let order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.cart.items.map(i=> {
